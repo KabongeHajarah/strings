4e743e69a3af5ddf793d2b0ff99c56148b8af05d
KaboneHajarah <Kabongehajarah@gmail.com>
2023-02-14 22:40:17 +0300

    changes-commit

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,49 @@
+fun main() {
+   strings()
+    me()
+    myDay()
+    name()
+}
+
+fun strings() {
+val school = "AkiraChix"
+    println(school[0])
+    println(school[2])
+    println(school[3])
+}
+
+fun me () {
+val name = "x"
+    val age = "y"
+    println("Hi,my name is $name and i am $age years old" )}
+
+fun myDay (){
+    val length = "tuesday"
+    println(length.length)
+}
+fun name (){
+    val name= "bella"
+    val names0= "bella"
+    val name1 = "rayan"
+    if (name ==names0){
+        println("Thats me")
+    }
+
+
+    if (name == name1){
+        println("Thats me")
+    }
+    else{
+        println("I don't know who that is")
+    }
+
+}
+
+    
+
+    
+
+
+
+
+
